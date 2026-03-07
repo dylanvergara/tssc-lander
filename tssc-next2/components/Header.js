8ca56b7932c604a693c98261f0822e38f735a373
dylanvergara <dylan@serialsales.co>
2026-03-07 17:31:23 -0600

    Delete tssc-next2 directory

--- tssc-next2/components/Header.js
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-  return (
-    <header className="header">
-      <img src="/images/logo-transparent.png" alt="The Serial Sales Community" className="header__logo" />
-      <span className="header__name">The Serial Sales Community</span>
-    </header>
-  );
-}
