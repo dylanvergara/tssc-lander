abe78d4c367561ea2b8cd2688cb44f7b85a8cae9
dylanvergara <dylan@serialsales.co>
2026-03-07 19:12:59 -0600

    Delete components directory

--- components/Header.js
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="header">
-      <img src="/images/logo-white-bg.png" alt="The Serial Sales Community" className="header__logo" />
-    </header>
-  );
-}
