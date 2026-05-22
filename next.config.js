036188f03d157e5ec24ce13e227426130bbc00b8
dylanvergara <dylan@serialsales.co>
2026-05-22 17:10:58 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/booking',
+        destination: 'https://links.serialagency.co/widget/bookings/book-application',
+        permanent: false,
+      },
+    ];
+  },
+};
+
 module.exports = nextConfig;
