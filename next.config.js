d77c91d7dee8a7b43cd381a63b7ca8ea252a0ad2
dylanvergara <dylan@serialsales.co>
2026-07-08 13:07:29 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: '/opportunity-cost-calculator', destination: 'https://opportunity-cost-calculator-tau.vercel.app/' },
+      { source: '/opportunity-cost-calculator/:path*', destination: 'https://opportunity-cost-calculator-tau.vercel.app/:path*' },
+    ];
+  },
+};
 
 module.exports = nextConfig;
